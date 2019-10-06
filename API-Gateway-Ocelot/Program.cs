1ba4c8318608343c9ee40d2f54978d2cf8c5a371
Venkatesh <vkalva45@gmail.com>
2019-10-06 14:20:35 -0500

    Create an API service that should be registered in Consul.

--- API-Gateway-Ocelot/Program.cs
+++ API-Gateway-Ocelot/Program.cs
@@ -11,7 +11,7 @@ namespace APIGateway
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+             CreateWebHostBuilder(args).Build().Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
